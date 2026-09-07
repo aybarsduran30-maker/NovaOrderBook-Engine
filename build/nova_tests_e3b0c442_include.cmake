bf75cd38e75d3e5c6f3a347982eb311ba99b2320
aybarsduran30-maker <aybarsduran30info@gmail.com>
2026-09-07 08:31:00 +0200

    chore: stop tracking build artifacts and binaries

--- build/nova_tests_e3b0c442_include.cmake
+++
@@ -1,3 +0,0 @@
-if(EXISTS "C:/Users/aybob/Desktop/NovaOrderBook-Engine/build/nova_tests_e3b0c442_${CTEST_CONFIGURATION_TYPE}_include.cmake")
-  include("C:/Users/aybob/Desktop/NovaOrderBook-Engine/build/nova_tests_e3b0c442_${CTEST_CONFIGURATION_TYPE}_include.cmake")
-endif()
